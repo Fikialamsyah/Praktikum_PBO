2174fc56d28f4b512721783f23443e00fd7bece9
Fiki Alamsyah <fikialamsyah16@gmai.com>
2020-10-15 14:03:17 +0700

    menambahkan folder Tugas_Praktikum_1

---
+++ Tugas_Praktikum_1/src/com/praktikum/tugas1/Tugas.java
@@ -0,0 +1,12 @@
+package com.praktikum.tugas1;
+
+public class Tugas {
+    public static void main(String[] args) {
+        System.out.println("==== Biodata ====");
+        System.out.println("Nama   : Fiki Alamsyah");
+        System.out.println("Alamat : Jalan Jaya 25");
+        System.out.println("Prodi  : Teknik Informatika");
+        System.out.println("Kelas  : TI02");
+        System.out.println("Moto   : Hadapi Cermati dan Lewati");
+    }
+}
